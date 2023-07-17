c88ec11c14eac3318486eca7830bb8103c9e78d8
Niklas Fulle <36989748+Salkin263@users.noreply.github.com>
2023-07-17 02:26:13 +0200

    Chapter 2: Basic Types

--- build/js/main.js
+++ build/js/main.js
@@ -1,1 +1,15 @@
 "use strict";
+let myName = "Niklas";
+let meaningOfLife;
+let isLoading;
+let album;
+myName = "Niklas Fulle";
+meaningOfLife = 42;
+isLoading = true;
+album = 1984;
+const sum = (a, b) => {
+    return a + b;
+};
+let postId;
+let isActive;
+let re = /\w+/g;
---
+++ src/main.ts
@@ -0,0 +1,19 @@
+let myName: string = "Niklas";
+let meaningOfLife: number;
+let isLoading: boolean;
+let album: any;
+
+
+myName = "Niklas Fulle";
+meaningOfLife = 42;
+isLoading = true;
+album = 1984;
+
+const sum = (a: number, b: string) => {
+  return a + b
+}
+
+let postId: string | number;
+let isActive: number | boolean;
+
+let re: RegExp = /\w+/g;
