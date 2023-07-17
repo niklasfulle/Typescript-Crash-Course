5fdfe2368d3396b67bb91a2ae31933ae9b97ecd2
Niklas Fulle <36989748+Salkin263@users.noreply.github.com>
2023-07-17 02:53:30 +0200

    Chapter 3: Arrays & Objects

--- build/js/main.js
+++ build/js/main.js
@@ -1,15 +1,47 @@
 "use strict";
-let myName = "Niklas";
-let meaningOfLife;
-let isLoading;
-let album;
-myName = "Niklas Fulle";
-meaningOfLife = 42;
-isLoading = true;
-album = 1984;
-const sum = (a, b) => {
-    return a + b;
+let stringArr = ['a', 'b', 'c'];
+let numberArr = ["1", 2, 3];
+let anyArr = [1, "a", true];
+stringArr[0] = "A";
+stringArr.push("d");
+numberArr[0] = 0;
+numberArr.push("4");
+anyArr[0] = false;
+anyArr.push(2);
+anyArr.unshift("b");
+let test = [];
+test.push("a");
+// Tuple
+let tuple = ["a", 1, true];
+let mixed = ["a", 1, true];
+// objects
+let myObj;
+myObj = [];
+console.log(typeof myObj);
+const person = {
+    name: "John",
+    alive: true
 };
-let postId;
-let isActive;
-let re = /\w+/g;
+person.name = "Jane";
+let test2 = {
+    name: "John",
+    alive: true,
+    albums: [1, "test"]
+};
+const greet = (person) => {
+    console.log("Hello", person.name.toLowerCase());
+};
+greet(test2);
+// Enums
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["USER"] = 1] = "USER";
+    Role[Role["READ_ONLY"] = 2] = "READ_ONLY";
+})(Role || (Role = {}));
+;
+const test3 = {
+    name: "John",
+    alive: true,
+    role: Role.ADMIN
+};
--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,64 @@
-let myName: string = "Niklas";
-let meaningOfLife: number;
-let isLoading: boolean;
-let album: any;
+let stringArr = ['a', 'b', 'c'];
+let numberArr = ["1", 2, 3];
+let anyArr = [1, "a", true];
+
+stringArr[0] = "A";
+stringArr.push("d");
+
+numberArr[0] = 0;
+numberArr.push("4");
+
+anyArr[0] = false;
+anyArr.push(2);
+anyArr.unshift("b");
+
+let test: string[] = [];
+test.push("a");
+
+// Tuple
+
+let tuple: [string, number, boolean] = ["a", 1, true];
+let mixed = ["a", 1, true];
 
 
-myName = "Niklas Fulle";
-meaningOfLife = 42;
-isLoading = true;
-album = 1984;
+// objects
 
-const sum = (a: number, b: string) => {
-  return a + b
+let myObj: object;
+myObj = [];
+console.log(typeof myObj);
+
+const person = {
+  name: "John",
+  alive: true
 }
 
-let postId: string | number;
-let isActive: number | boolean;
+person.name = "Jane";
 
-let re: RegExp = /\w+/g;
+interface Person {
+  name: string,
+  alive: boolean
+  albums: (string | number)[]
+}
+
+let test2: Person = {
+  name: "John",
+  alive: true,
+  albums: [1, "test"]
+}
+
+const greet = (person: Person) => {
+  console.log("Hello", person.name.toLowerCase());
+}
+
+greet(test2);
+
+// Enums
+
+enum Role { ADMIN, USER, READ_ONLY };
+
+const test3 = {
+  name: "John",
+  alive: true,
+  role: Role.ADMIN
+
+}
